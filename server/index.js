e7fb2af5134cba393e2c6ad42a09e1c11367c36e
vladovoev <vladovoev@yahoo.co.uk>
2018-05-21 14:41:08 +0300

    added google OATH

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
-const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./config/keys')
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'Hello from heroku' });
+const app = express();
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken) => {
+        console.log(accessToken);
+    })
+)
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}), (number) => {
+    console.log(number);
 })
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => { console.log(`Server is up and running on port ${PORT}`) });
