49486fee35bc78eaae389548c39b0500ff98d0ba
vladovoev <vladovoev@yahoo.co.uk>
2018-05-22 16:16:44 +0300

    added mongodb and get user route

--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const passportConfig = require('./services/passport.js')
-const keys = require('./config/keys')
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
 require('./models/User');
+require('./services/passport');
 
-let server;
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 + 1000,
+        keys: [keys.cookieKey]
+    })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,19 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys')
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    })
+})
 
 passport.use(
     new GoogleStrategy({
@@ -8,5 +21,17 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({ googleId: profile.id })
+            .then((isExistingUser) => {
+                if (isExistingUser) {
+                    done(null, isExistingUser);
+                } else {
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then((user) => {
+                            done(null, user)
+                        })
+                }
+            })
     })
 )
