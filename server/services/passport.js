606609f0f8e3016173f089b3b97345ad6f0d9881
vladovoev <vladovoev@yahoo.co.uk>
2018-05-23 13:43:05 +0300

    x

--- server/services/passport.js
+++ server/services/passport.js
@@ -19,8 +19,7 @@ passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: 'https://sleepy-brushlands-59184.herokuapp.com/auth/google/callback',
-        proxy: true
+        callbackURL: '/auth/google/callback',
     }, (accessToken, refreshToken, profile, done) => {
         User.findOne({ googleId: profile.id })
             .then((isExistingUser) => {
