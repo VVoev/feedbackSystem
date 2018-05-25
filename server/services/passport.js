913eb647490ef1829fdc38635c28a7a78e7c8274
vladovoev <vladovoev@yahoo.co.uk>
2018-05-25 11:51:50 +0300

    added async/await

--- server/services/passport.js
+++ server/services/passport.js
@@ -21,18 +21,14 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback',
         proxy: true
-    }, (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId: profile.id })
-            .then((isExistingUser) => {
-                if (isExistingUser) {
-                    done(null, isExistingUser);
-                } else {
-                    new User({ googleId: profile.id })
-                        .save()
-                        .then((user) => {
-                            done(null, user)
-                        })
-                }
-            })
+    }, async (accessToken, refreshToken, profile, done) => {
+        const isExistingUser = await User.findOne({ googleId: profile.id })
+
+        if (isExistingUser) {
+            return done(null, isExistingUser);
+        }
+
+        const user = await new User({ googleId: profile.id }).save()
+        done(null, user);
     })
 )
